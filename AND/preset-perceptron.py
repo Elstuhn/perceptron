649bf575ed84c299ba4c3634193b0ec2477cdb0b
Elston Tan <elstuhnofficial@gmail.com>
2022-12-30 14:54:16 +0800

    Update preset-perceptron.py

--- AND/preset-perceptron.py
+++ AND/preset-perceptron.py
@@ -1,8 +1,8 @@
 from ../model.py import *
 
 net = NeuralNetwork(2)
-net.setweights(1, [1, 1])
-print(net.test(np.array([1, 1]), -1.5))
-print(net.test(np.array([1, 0]), -1.5))
-print(net.test(np.array([0, 1]), -1.5))
-print(net.test(np.array([0, 0]), -1.5))
+net.setweights(-1.5, [1, 1])
+print(net.test(np.array([1, 1])))
+print(net.test(np.array([1, 0])))
+print(net.test(np.array([0, 1])))
+print(net.test(np.array([0, 0])))
