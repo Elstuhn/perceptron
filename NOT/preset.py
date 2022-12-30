a4ec62b8e451c2f6840b7d1865825288571433f1
Elston Tan <elstuhnofficial@gmail.com>
2022-12-30 14:53:47 +0800

    Update preset.py

--- NOT/preset.py
+++ NOT/preset.py
@@ -1,6 +1,6 @@
 from ../model.py import *
 
 net = NeuralNetwork(1)
-net.setweights(1, [-1])
-print(net.test(np.array([1]), 0.5))
-print(net.test(np.array([0]), 0.5))
+net.setweights(.5, [-1])
+print(net.test(np.array([1])))
+print(net.test(np.array([0])))
